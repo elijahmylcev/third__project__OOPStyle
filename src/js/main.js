44a356a4f70803a56fbe52c4be644103163ef6e5
Elijah <elijahmylcev@gmail.com>
2021-12-12 14:02:20 +0300

    playVideo with youtube api

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
+import VideoPlayer from './modules/playVideo';
 import Slider from './modules/slider';
 
 window.addEventListener('DOMContentLoaded', () => {
   const slider = new Slider('.page', '.next');
   slider.render();
+
+  const player = new VideoPlayer('.showup .play', '.overlay', '.close');
+  player.init();
 });
