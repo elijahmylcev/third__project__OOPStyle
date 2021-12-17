eecde7d33f4a0726c9f4fd90e0f9a573aade8b98
Elijah <elijahmylcev@gmail.com>
2021-12-17 03:57:35 +0300

    Difference module

--- docs/script.js
+++ docs/script.js
@@ -2833,18 +2833,31 @@ function () {
 
     this.oldOfficer = document.querySelector(oldOfficer);
     this.newOfficer = document.querySelector(newOfficer);
-    this.items = items;
+    this.oldItems = this.oldOfficer.querySelectorAll(items);
+    this.newItems = this.newOfficer.querySelectorAll(items);
+    this.oldCounter = 0;
+    this.newCounter = 0;
   }
 
   _createClass(Difference, [{
-    key: "hideItems",
-    value: function hideItems() {
-      this.oldOfficer.querySelectorAll(this.items).forEach(function (item, i, arr) {
-        if (i !== arr.length - 1) {
-          item.style.display = 'none';
+    key: "bindTriggers",
+    value: function bindTriggers(container, items, counter) {
+      container.querySelector('.plus').addEventListener('click', function () {
+        if (counter !== items.length - 2) {
+          items[counter].classList.add('animated', 'fadeInDown');
+          items[counter].style.display = 'flex';
+          counter++;
+        } else {
+          items[counter].classList.add('animated', 'fadeInDown');
+          items[counter].style.display = 'flex';
+          items[items.length - 1].remove();
         }
       });
-      this.newOfficer.querySelectorAll(this.items).forEach(function (item, i, arr) {
+    }
+  }, {
+    key: "hideItems",
+    value: function hideItems(items) {
+      items.forEach(function (item, i, arr) {
         if (i !== arr.length - 1) {
           item.style.display = 'none';
         }
@@ -2853,7 +2866,10 @@ function () {
   }, {
     key: "init",
     value: function init() {
-      this.hideItems();
+      this.hideItems(this.oldItems);
+      this.hideItems(this.newItems);
+      this.bindTriggers(this.oldOfficer, this.oldItems, this.oldCounter);
+      this.bindTriggers(this.newOfficer, this.newItems, this.newCounter);
     }
   }]);
 
--- src/js/modules/difference.js
+++ src/js/modules/difference.js
@@ -2,17 +2,29 @@ export default class Difference {
   constructor(oldOfficer, newOfficer, items) {
     this.oldOfficer = document.querySelector(oldOfficer);
     this.newOfficer = document.querySelector(newOfficer);
-    this.items = items;
+    this.oldItems = this.oldOfficer.querySelectorAll(items);
+    this.newItems = this.newOfficer.querySelectorAll(items);
+    this.oldCounter = 0;
+    this.newCounter = 0;
   }
 
-  hideItems() {
-    this.oldOfficer.querySelectorAll(this.items).forEach((item, i, arr) => {
-      if (i !== arr.length - 1) {
-        item.style.display = 'none';
+  bindTriggers(container, items, counter) {
+    container.querySelector('.plus').addEventListener('click', () => {
+      if (counter !== items.length - 2) {
+        items[counter].classList.add('animated', 'fadeInDown');
+        items[counter].style.display = 'flex';
+        counter++;
+      } else {
+        items[counter].classList.add('animated', 'fadeInDown');
+
+        items[counter].style.display = 'flex';
+        items[items.length - 1].remove();
       }
     });
+  }
 
-    this.newOfficer.querySelectorAll(this.items).forEach((item, i, arr) => {
+  hideItems(items) {
+    items.forEach((item, i, arr) => {
       if (i !== arr.length - 1) {
         item.style.display = 'none';
       }
@@ -20,6 +32,9 @@ export default class Difference {
   }
 
   init() {
-    this.hideItems();
+    this.hideItems(this.oldItems);
+    this.hideItems(this.newItems);
+    this.bindTriggers(this.oldOfficer, this.oldItems, this.oldCounter);
+    this.bindTriggers(this.newOfficer, this.newItems, this.newCounter);
   }
 }
